11c87717681ad6185fd1cc36f758799b0416741b
PoushnayaA <poushnaya@bk.ru>
2023-08-09 14:13:08 +0300

    Смена языка, анимация, состояния (кроме формы)

--- source/js/main.js
+++ source/js/main.js
@@ -1,59 +1,64 @@
 const member_btn = document.querySelector('.main-header__button');
 const form = document.querySelector('.form-wrapper');
+
 const scrollWindow = () => {
   form.scrollIntoView();
 };
 const onBeMemberButtonClick = () => scrollWindow();
+
 member_btn.addEventListener('click', onBeMemberButtonClick);
+
+
+
+
 
 const header = document.querySelector('.main-header');
 const pagination = document.querySelectorAll('.slider-pagination__item');
-console.log(pagination[0]);
+
+const PHOTO = [
+  'bmw',
+  'volvo',
+  'mini',
+  'infinity'
+]
 
 const changePhoto = () => {
-  if (header.classList.contains('main-header--bmw')) {
-    header.classList.remove('main-header--bmw');
-    header.classList.add('main-header--volvo');
-    pagination[0].querySelector('.slider-pagination__button').classList.remove('slider-pagination__button--active');
-    pagination[1].querySelector('.slider-pagination__button').classList.add('slider-pagination__button--active');
-  } else if (header.classList.contains('main-header--volvo')) {
-    header.classList.remove('main-header--volvo');
-    header.classList.add('main-header--mini');
-    pagination[1].querySelector('.slider-pagination__button').classList.remove('slider-pagination__button--active');
-    pagination[2].querySelector('.slider-pagination__button').classList.add('slider-pagination__button--active');
-  } else if (header.classList.contains('main-header--mini')) {
-    header.classList.remove('main-header--mini');
-    header.classList.add('main-header--infinity');
-    pagination[2].querySelector('.slider-pagination__button').classList.remove('slider-pagination__button--active');
-    pagination[3].querySelector('.slider-pagination__button').classList.add('slider-pagination__button--active');
-  } else if (header.classList.contains('main-header--infinity')) {
-    header.classList.remove('main-header--infinity');
-    header.classList.add('main-header--bmw');
-    pagination[3].querySelector('.slider-pagination__button').classList.remove('slider-pagination__button--active');
-    pagination[0].querySelector('.slider-pagination__button').classList.add('slider-pagination__button--active');
-  }
-  return
+  for (let i = 0; true; i++) {
+    if (header.classList.contains(`main-header--${PHOTO[i%4]}`)) {
+      header.classList.remove(`main-header--${PHOTO[i%4]}`);
+      header.classList.add(`main-header--${PHOTO[(i+1)%4]}`);
+      pagination[i%4].querySelector('.slider-pagination__button').classList.remove('slider-pagination__button--active');
+      pagination[(i+1)%4].querySelector('.slider-pagination__button').classList.add('slider-pagination__button--active');
+      return
+    }
+  };
+
 }
 
 setInterval(changePhoto, 5000);
 
 
-// const SLIDER_PHOTOS = {
-//   0: 'url("../../img/bg-infinity2x.webp")',
-//   1: 'url("../../img/bg-infinity2x.webp")',
-//   2: 'url("../../img/bg-infinity2x.webp")',
-//   3: 'url("../../img/bg-infinity2x.webp")'
-// };
 
-// const pagination = document.querySelectorAll('.slider-pagination__item');
-// const header = document.querySelector('.main-header');
 
-// const onPaginationButtonClick = (index) => {
-//   header.style.backgroundImage = SLIDER_PHOTOS.index;
-//   console.log(index);
-//   console.log(header.style.backgroundImage);
-// };
 
-// for (let i=0; i < pagination.length; i++) {
-//   pagination[i].addEventListener('click', onPaginationButtonClick(i))
-// };
+const language = document.querySelector('.change-language__button');
+const lang_buttons = document.querySelector('.change-language__select');
+
+language.addEventListener('click', function () {
+  document.querySelector('.change-language__select').classList.toggle('visually-hidden');
+  language.classList.toggle('change-language__button--active');
+});
+
+lang_buttons.addEventListener('click', function (evt) {
+  document.querySelector('.change-language__select').classList.add('visually-hidden');
+  document.querySelector('.change-language__item--active').classList.remove('change-language__item--active');
+  language.classList.remove('change-language__button--active');
+  if (evt.target.matches('.change-language__item')) {
+    evt.target.classList.add('change-language__item--active');
+    language.textContent = evt.target.textContent;
+    language.textContent.style.textTransform = uppercase;
+  }
+});
+
+
+
